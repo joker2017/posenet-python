b68dbadd02b486533707da70a9bd3207d2e5d661
joker2017 <depsemt2009@ya.ru>
2019-10-31 00:37:46 +0300

    Update Python2CoreML.py

--- posenet/converter/Python2CoreML.py
+++ posenet/converter/Python2CoreML.py
@@ -6,7 +6,7 @@ from keras.preprocessing.image import load_img
 import tfcoreml
 import coremltools
 import yaml
-def convert2(imageSize, versionName)
+def convert2()
     cfg = load_config()
     checkpoints = cfg['checkpoints']
     image_size = cfg['imageSize']
