5e9b387d130dc27b560d77cf08afce7ed19f4e7c
joker2017 <depsemt2009@ya.ru>
2019-10-31 02:25:13 +0300

    Update python2coreml.py

--- posenet/converter/python2coreml.py
+++ posenet/converter/python2coreml.py
@@ -8,7 +8,7 @@ import coremltools
 import posenet
 from posenet.model import model_id_to_ord, load_config
 from posenet.converter.config import load_config
-def convert2(model_id):
+def convert2(model_id, model_dir):
     model_ord = model_id_to_ord(model_id)
     cfg = load_config()
     print(cfg)
